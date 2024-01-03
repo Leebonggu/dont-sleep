f5711bee40d88bfc6ae982f5994304ab039babfe
Leebonggu <leebonggu12@hanmail.net>
2024-01-03 13:25:28 +0900

    push

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react'
+import { 쓴소리들 } from './const';
+
+function getOne(데이터배열: string[]) {
+  const index = Math.floor(Math.random() * 데이터배열.length);
+  const target = 데이터배열[index];
+
+  return target;
+}
+
+
+function App() {
+  const [lang, setLang] = useState(getOne(쓴소리들))
+
+  useEffect(() => {
+    setInterval(() => {
+      setLang(getOne(쓴소리들));
+  }, 10000); 
+  }, [])
+
+  return (
+    <div style={{ width: '100vw', height: '100vh', display: 'flex', background:'black'}}>
+      <div style={{flex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+        <h2 style={{ color: 'white'}}>{lang}</h2>
+      </div>
+    </div>
+  )
+}
+
+export default App
